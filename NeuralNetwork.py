5435ae7ccfb6b1e5bb0434e6e819030bd4e7f390
Sahalu Navon <navon.sahalu@intel.com>
2023-04-08 18:50:41 +0300

    accuracy and loss foe nn

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -3,6 +3,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.utils import check_random_state
 from utils import save_confusion_matrix
+import matplotlib.pyplot as plt
 
 
 class neural_network:
@@ -10,7 +11,7 @@ class neural_network:
 
         # create an MLPClassifier with 1 hidden layer with 10 neurons
         nn = MLPClassifier(hidden_layer_sizes=(
-            10,), max_iter=1000, random_state=rs)
+            10,), max_iter=1000, early_stopping=True, n_iter_no_change=50, random_state=rs)
 
         # fit the model to the training data
         nn.fit(X_train, y_train)
@@ -23,3 +24,16 @@ class neural_network:
         print(classification_report(y_test, y_pred))
         save_confusion_matrix(
             y_test, y_pred, [0, 1], "out/NeuralNetwork/confusion_matrix.png")
+        self.save_accuracy_and_loss_graphs(
+            nn.validation_scores_, nn.loss_curve_)
+
+    def save_accuracy_and_loss_graphs(self, accuracy, loss) -> None:
+        plt.clf()
+        plt.plot(accuracy)
+        plt.title("Accuracy")
+        plt.savefig("out/NeuralNetwork/Accuracy.png")
+
+        plt.clf()
+        plt.plot(loss)
+        plt.title("Loss")
+        plt.savefig("out/NeuralNetwork/Loss.png")
