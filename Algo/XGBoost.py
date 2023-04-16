5bd26f2a9306f1ebc228912618e39ce9afad8f46
NavonSahalu <navon.sahalu@intel.com>
2023-04-16 11:21:05 +0300

    xgboost

---
+++ Algo/XGBoost.py
@@ -0,0 +1,27 @@
+import numpy as np
+from xgboost import XGBClassifier
+from utils import save_confusion_matrix, save_ROC
+
+
+class XGBoost:
+    def __init__(self, X_train, X_test, y_train, y_test, rs, out_folder, is_best_threshold=False) -> None:
+
+        # define XGBoost model
+        model = XGBClassifier()
+
+        # train XGBoost model
+        model.fit(X_train, y_train)
+
+        # make predictions on test data
+        y_pred = model.predict(X_test)
+
+        threshold = 0.5
+        y_pred_binary = np.where(y_pred >= threshold, 1, 0)
+
+        best_threshold = save_ROC('XGBoost',
+                                  f"{out_folder}/XGBoost/ROC.png", y_test, y_pred)
+        if is_best_threshold:
+            y_pred_binary = np.where(y_pred >= best_threshold, 1, 0)
+
+        save_confusion_matrix(y_test, np.round(y_pred_binary).astype(
+            int), [0, 1], f"{out_folder}/XGBoost/confusion_matrix.png", 'XGBoost')
--- main.py
+++ main.py
@@ -6,25 +6,28 @@ from Algo.RandomForest import random_forest
 from Algo.DecisionTree import decision_tree
 from Algo.NeuralNetwork import neural_network
 from Algo.PolynomialSVM import PolynomialSVM
+from Algo.XGBoost import XGBoost
 
 if __name__ == "__main__":
     data = DataLoader("DiabetsPredict.csv")
-    models = [PolynomialSVM, linear_regression, logistic_regression, polynominal_regression, decision_tree, random_forest, neural_network]
+    models = [PolynomialSVM, linear_regression, logistic_regression,
+              polynominal_regression, decision_tree, random_forest, neural_network, XGBoost]
 
     # First run, raw data
     X, y = data.get_raw_data()
     X_train, X_test, y_train, y_test, rs = data.get_train_test_norm(X, y)
     for model in models:
         model(X_train, X_test, y_train, y_test, rs, 'raw_out')
-    
+
     # Second run, clean data
     X, y = data.get_clean_data()
     X_train, X_test, y_train, y_test, rs = data.get_train_test_norm(X, y)
     for model in models:
         model(X_train, X_test, y_train, y_test, rs, 'clean_out')
-    
+
     # Third run, clean data and best threshold
     X, y = data.get_clean_data()
     X_train, X_test, y_train, y_test, rs = data.get_train_test_norm(X, y)
     for model in models:
-        model(X_train, X_test, y_train, y_test, rs, 'best_out', is_best_threshold = True)
+        model(X_train, X_test, y_train, y_test, rs,
+              'best_out', is_best_threshold=True)
